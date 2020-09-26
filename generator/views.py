669a600284d037c57fcc480a042f91c91eeed22c
Josh <jyi468@gmail.com>
2020-09-26 11:46:05 -0400

    Add urls and views

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Hello there friend!')  # must wrap in HttpResponse to return
+
+
+def eggs(request):
+    return HttpResponse('<h1>Eggs are so tasty</h1>')
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from generator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),  # interacting with DB, admin stuff
+    path('', views.home),
+    path('eggs', views.eggs)  # prepends the '/' character
 ]
