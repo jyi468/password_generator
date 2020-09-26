80a868d10e38c18ef65dd1d273b88a4b98455464
Josh <jyi468@gmail.com>
2020-09-26 13:20:20 -0400

    Add password generation logic

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 # render(request, path, data) function allows you to pass in a template and show that
 
 
@@ -9,7 +10,24 @@ def home(request):
 
 
 def password(request):
-    return render(request, 'generator/password.html')
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))  # extend is the same as using + to concatenate lists
+
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    length = int(request.GET.get('length', 12))  # GET - http verb. You can get the query parameters
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': thepassword})
 
 
 def eggs(request):
